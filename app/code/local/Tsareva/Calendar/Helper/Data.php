4293f2874537196a028ed24c9d39793cb4805e20
Alena Tsareva <alena.tsareva@webinse.com>
2013-10-18 12:54:08 +0300

    added config and helper

---
+++ app/code/local/Tsareva/Calendar/Helper/Data.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Data helper
+ * 
+ * @category   Tsareva
+ * @package    Tsareva_Calendar
+ * @author     Tsareva Alena <tsareva.as@gmail.com>
+ */
+class Tsareva_Calendar_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
