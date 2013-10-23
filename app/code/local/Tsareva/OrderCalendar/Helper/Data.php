3d77423ffe1e2d26980a794f3cd19605933ec219
Alena Tsareva <alena.tsareva@webinse.com>
2013-10-23 14:29:20 +0300

    changed module name

--- app/code/local/Tsareva/OrderCalendar/Helper/Data.php
+++ app/code/local/Tsareva/OrderCalendar/Helper/Data.php
@@ -4,10 +4,10 @@
  * Data helper
  * 
  * @category   Tsareva
- * @package    Tsareva_Calendar
+ * @package    Tsareva_OrderCalendar
  * @author     Tsareva Alena <tsareva.as@gmail.com>
  */
-class Tsareva_Calendar_Helper_Data extends Mage_Core_Helper_Abstract
+class Tsareva_OrderCalendar_Helper_Data extends Mage_Core_Helper_Abstract
 {
 
 }
